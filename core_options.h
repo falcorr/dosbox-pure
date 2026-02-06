4ad50fb86e5c3b1163a0e2ccf853474b99c85569
falcorr <65019252+falcorr@users.noreply.github.com>
2026-02-06 19:20:53 +1100

    Update core_options.h

--- core_options.h
+++ core_options.h
@@ -302,15 +302,16 @@ static retro_core_option_v2_definition option_defs[DBP_Option::_OPTIONS_TOTAL] =
 		"You can also force it to open by holding shift or L2/R2 when selecting 'Restart'.", NULL,
 		DBP_OptionCat::General,
 		{
-			{ "99", "Show at start, show again after game exit (default)" },
+			//{ "99", "Show at start, show again after game exit (default)" },
 #ifndef STATIC_LINKING
-			{ "5", "Show at start, shut down core 5 seconds after auto started game exit" },
-			{ "3", "Show at start, shut down core 3 seconds after auto started game exit" },
+			//{ "5", "Show at start, shut down core 5 seconds after auto started game exit" },
+			//{ "3", "Show at start, shut down core 3 seconds after auto started game exit" },
 			{ "0", "Show at start, shut down core immediately after auto started game exit" },
 #endif
-			{ "-1", "Always show menu on startup and after game exit, ignore auto start setting" },
+			//{ "-1", "Always show menu on startup and after game exit, ignore auto start setting" },
 		},
-		"99"
+		//"99"
+        "0"
 	},
 	{
 		"dosbox_pure_menu_transparency",
